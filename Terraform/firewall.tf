1693d1d2c0849944e775fc8bcfc59c958dd4d131
Sasha Alimov <aalimov@axcient.com>
2018-11-05 23:08:27 +0300

    terraform fmt

--- Terraform/firewall.tf
+++ Terraform/firewall.tf
@@ -4,15 +4,16 @@ resource "google_compute_firewall" "default" {
 
   allow {
     protocol = "tcp"
-    ports = ["9292"]
+    ports    = ["9292"]
   }
-  target_tags = ["puma-server"]
+
+  target_tags   = ["puma-server"]
   source_ranges = ["0.0.0.0/0"]
 
- # allow {
- #   protocol = "tcp"
- #   ports    = ["80", "8080", "1000-2000"]
- # }
+  # allow {
+  #   protocol = "tcp"
+  #   ports    = ["80", "8080", "1000-2000"]
+  # }
 
-#  source_tags = ["puma-server"]
+  #  source_tags = ["puma-server"]
 }
