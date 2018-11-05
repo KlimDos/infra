2db93d05ff6f9db4df13b2097b4102e9408470a8
Sasha Alimov <aalimov@axcient.com>
2018-11-06 01:03:57 +0300

    manage ssh firewall rule; add static ip address

--- Terraform/firewall.tf
+++ Terraform/firewall.tf
@@ -1,4 +1,4 @@
-resource "google_compute_firewall" "default" {
+resource "google_compute_firewall" "puma9292" {
   name    = "puma9292"
   network = "default"
 
@@ -9,11 +9,20 @@ resource "google_compute_firewall" "default" {
 
   target_tags   = ["puma-server"]
   source_ranges = ["0.0.0.0/0"]
-
-  # allow {
-  #   protocol = "tcp"
-  #   ports    = ["80", "8080", "1000-2000"]
-  # }
-
+}
+resource "google_compute_firewall" "firewall_ssh" {
+  name    = "default-allow-ssh"
+  network = "default"
+  description = "Allow SSH to the instance from anywhere"
+  allow {
+     protocol = "tcp"
+     ports    = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
   #  source_tags = ["puma-server"]
+}
+
+resource "google_compute_address" "app_ip" {
+  name = "reddit-app-ip"
+  network_tier = "STANDARD"
 }
