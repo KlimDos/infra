490b05ac0fda26ff3a89a13c08b5b6fba4726186
Sasha Alimov <aalimov@axcient.com>
2018-11-08 19:25:29 +0300

    everything seems to work; modules added

---
+++ Terraform/modules/app/main.tf
@@ -0,0 +1,46 @@
+resource "google_compute_instance" "app" {
+  name         = "reddit-app"
+  machine_type = "g1-small"
+  zone         = "europe-west1-b"
+  tags         = ["reddit-app"]
+
+  #old fashen style of deploy the fresh application code
+  #metadata_startup_script = "${file("../deploy.sh")}"
+  metadata {
+    sshKeys = "aalimov:${file(var.public_key_path)}"
+  }
+
+  # определение загрузочного диска2
+  boot_disk {
+    initialize_params {
+      image = "${var.app_disk_image}"
+    }
+  }
+
+  # определение сетевого интерфейса
+  network_interface {
+    # сеть, к которой присоединить данный интерфейс
+    network = "default"
+    # использовать ephemeral IP для доступа из Интернет
+    access_config {
+      network_tier = "STANDARD"
+      nat_ip = "${google_compute_address.app_ip.address}"
+    }
+  }
+}
+resource "google_compute_address" "app_ip" {
+  name = "reddit-app-ip"
+  network_tier = "STANDARD"
+}
+resource "google_compute_firewall" "reddit-app" {
+  name    = "reddit-app"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  target_tags   = ["reddit-app"]
+  source_ranges = ["0.0.0.0/0"]
+}
