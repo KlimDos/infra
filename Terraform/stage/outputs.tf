c84c9cfdf55ff51119244802728d266d8f9afd13
KlimDos <klimdos@gmail.com>
2018-11-18 15:20:59 +0300

    alot of changes; app works; two separete instance

--- Terraform/stage/outputs.tf
+++ Terraform/stage/outputs.tf
@@ -1,8 +1,8 @@
 output "app_external_ip" {
   #value = "${google_compute_instance.app.network_interface.0.access_config.0.nat_ip}"
-  value = "${module.app_for_full_img.app_external_ip}"
+  value = "${module.app.app_external_ip}"
 }
 
-/*output "db_local_ip" {
+output "db_local_ip" {
   value = "${module.db.db_internal_ip}"
-}*/
+}
